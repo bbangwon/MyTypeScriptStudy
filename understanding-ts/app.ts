1486fd8bff2cb9a88719a9a2c1968afa7d2ef653
youngil <youngil@placeb.com>
2024-05-28 10:04:42 +0900

    배열타입

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -11,6 +11,16 @@
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"]
 };
 
+//배열 타입
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
