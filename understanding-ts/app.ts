da8c1ac820fa38f0bdca33a6b6796941a7222fdd
youngil <youngil@placeb.com>
2024-05-28 15:58:26 +0900

    타입의 기능을 하는 함수

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,34 +1,24 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
 
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2; // parseFloat(input1) + parseFloat(input2)
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-
-  // if (resultConversion === "as-number") {
-  //   return +result; // parseFloat(result)
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+// void 리턴 타입은 아무것도 반환하지 않는 함수에 사용
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
+// undefined 리턴 타입은 아무것도 반환하지 않는 함수에 사용
+function printResultUndefined(num: number): undefined {
+    console.log('Result: ' + num);
+    return;
+  }
 
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
+let someValue: undefined;
