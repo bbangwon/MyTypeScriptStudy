f5292ca7d66c806d60dfe070ac2365b236c2db46
youngil <youngil@placeb.com>
2024-05-28 16:20:26 +0900

    함수 타입 및 콜백

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,18 +1,23 @@
-
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
 // void 리턴 타입은 아무것도 반환하지 않는 함수에 사용
 function printResult(num: number): void {
-  console.log('Result: ' + num);
+  console.log("Result: " + num);
 }
 
 // undefined 리턴 타입은 아무것도 반환하지 않는 함수에 사용
 function printResultUndefined(num: number): undefined {
-    console.log('Result: ' + num);
-    return;
-  }
+  console.log("Result: " + num);
+  return;
+}
+
+// 콜백함수의 리턴 타입을 void로 지정하면 콜백함수에서 반환하는 값은 무시된다.
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
 printResult(add(5, 12));
 
@@ -20,5 +25,12 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, result => {
+  console.log(result);
+  return result;
+});
+
+addAndHandle(10, 20, printResult);
 
 let someValue: undefined;
