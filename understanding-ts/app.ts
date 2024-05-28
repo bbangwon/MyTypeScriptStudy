8620851c55fcd0732c6152044c57cbd4918f23d4
youngil <youngil@placeb.com>
2024-05-28 10:59:10 +0900

    열거형으로 작업하기

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -8,21 +8,26 @@
 // };
 
 // 오브젝트 타입 추론
-const person : {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //제대로 타입을 지정해주기 위해 튜플 사용, 타입지정이 없을 경우 유니온 타입으로 추론됨
-} = {  
+// const person : {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //제대로 타입을 지정해주기 위해 튜플 사용, 타입지정이 없을 경우 유니온 타입으로 추론됨
+// } = {  
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 0, AUTHOR }; //enum은 숫자 또는 문자열 값 집합을 정의하는 강력한 방법
+
+const person = {  
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
-
-
-//push는 튜플에도 작동함 (실제로는 튜플이 아닌 배열이라서 가능) - 타입스크립트가 잡지 못함
-person.role.push("admin");  
 
 // 튜플의 길이가 정해져 있기 때문에 에러 발생
 //person.role = [0, "admin", "user"]; // !!! ERROR !!!  
@@ -37,4 +42,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if(person.role === Role.ADMIN) {
+  console.log('is admin');
 }
