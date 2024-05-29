588a3fc6f4684dcf5752f36a529d0f6e720db6e7
youngil <youngil@placeb.com>
2024-05-29 10:41:40 +0900

    TypeScript 핵심 라이브러리 이해하기

--- typescript-compiler/app.ts
+++ typescript-compiler/app.ts
@@ -1,2 +1,5 @@
-const userName = 'Max';
-console.log(userName);
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", () => {
+  console.log("Button clicked!");
+});
