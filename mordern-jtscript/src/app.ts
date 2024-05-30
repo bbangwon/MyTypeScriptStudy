c40478f91db8e1b547116d150018b24fb00ccdf6
youngil <youngil@placeb.com>
2024-05-30 11:05:37 +0900

    화살표 함수, 함수 기본 매개변수

--- mordern-jtscript/src/app.ts
+++ mordern-jtscript/src/app.ts
@@ -1,7 +1,12 @@
-function add(n1: number, n2: number) {
-  result = n1 + n2;
-  var result;
-  return result;
-}
+// function add(n1: number, n2: number) {
+//   result = n1 + n2;
+//   var result;
+//   return result;
+// }
 
-console.log(add(5, 10));
+const add = (n1: number, n2: number) => n1 + n2;
+const addOrAddOne = (n1: number, n2: number = 1) => n1 + n2;
+const printOutput: (a: number | string) => void = console.log;
+
+printOutput(add(5, 2));
+printOutput(addOrAddOne(5));
