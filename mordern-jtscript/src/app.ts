2556e32fb854d71e80cda00524cb9346579fe7a8
youngil <youngil@placeb.com>
2024-05-30 13:57:12 +0900

    나머지 매개변수

--- mordern-jtscript/src/app.ts
+++ mordern-jtscript/src/app.ts
@@ -4,22 +4,30 @@
 //   return result;
 // }
 
-const add = (n1: number, n2: number) => n1 + n2;
-const addOrAddOne = (n1: number, n2: number = 1) => n1 + n2;
-const printOutput: (a: number | string) => void = console.log;
+// const add = (n1: number, n2: number) => n1 + n2;
+// const addOrAddOne = (n1: number, n2: number = 1) => n1 + n2;
+// const printOutput: (a: number | string) => void = console.log;
 
-printOutput(add(5, 2));
-printOutput(addOrAddOne(5));
+// printOutput(add(5, 2));
+// printOutput(addOrAddOne(5));
 
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking'];
+// const hobbies = ['Sports', 'Cooking'];
+// const activeHobbies = ['Hiking'];
 
-activeHobbies.push(...hobbies);
+// activeHobbies.push(...hobbies);
 
-const person = {
-  firstName: 'Max',
-  age: 30,
+// const person = {
+//   firstName: 'Max',
+//   age: 30,
+// };
+
+// const copiedPerson = { ...person }; 
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
 };
 
-const copiedPerson = { ...person }; 
-
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
