dfba95b4a725545c716c25b4aa384300db40efd2
정영일 <bbangwon@outlook.com>
2024-06-02 00:44:43 +0900

    문서 서식 맞춤

--- generic/src/app.ts
+++ generic/src/app.ts
@@ -4,18 +4,19 @@ const names: Array<string> = []; //string[]
 
 //프로미스 타입
 const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  }, 2000);
+    setTimeout(() => {
+        resolve('This is done!');
+    }, 2000);
 });
 
 promise.then(data => {
-  data.split(' ');  
+    data.split(' ');
 });
 
 // 제너릭 함수
 function merge<T extends {}, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB);}
+    return Object.assign(objA, objB);
+}
 
-const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30});
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergedObj);
