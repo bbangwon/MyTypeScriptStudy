cb757832a62d1093e17e0efd297363b84f4155eb
youngil <youngil@placeb.com>
2024-06-01 13:30:14 +0900

    클래스와 인터페이스 사용하기

--- class-study/src/app.ts
+++ class-study/src/app.ts
@@ -1,17 +1,22 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
-user1 = {
-  name: "Max",
-  age: 30,
-  greet(phrase: string) {
-    console .log(phrase + " " + this.name);
-  },
-};
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);  
+  }
+}
+
+let user1: Person;
+user1 = new Person('Max');
 user1.greet("Hi there - I am");
