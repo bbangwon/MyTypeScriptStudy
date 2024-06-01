d04f4baa383bf0f52802782799230b0422012676
BreadOne <bbangwon.chung@gmail.com>
2024-06-02 08:28:54 +0900

    다른 일반 함수

--- generic/src/app.ts
+++ generic/src/app.ts
@@ -4,19 +4,37 @@ const names: Array<string> = []; //string[]
 
 //프로미스 타입
 const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
 });
 
-promise.then(data => {
-    data.split(' ');
+promise.then((data) => {
+  data.split(" ");
 });
 
 // 제너릭 함수
 function merge<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+  return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, {age: 30});
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj);
+
+//다른 일반 함수
+//문자든 배열이든 상관없지만 length가 있어야 한다.
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 elements.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
