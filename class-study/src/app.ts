f8fd02416be923ee37095dec4d9489916833990d
youngil <youngil@placeb.com>
2024-06-01 13:51:28 +0900

    선택적 매개변수 & 속성

--- class-study/src/app.ts
+++ class-study/src/app.ts
@@ -3,32 +3,39 @@ interface AddFn {
   (a: number, b: number): number;
 }
 let add: AddFn;
-add = (n1: number, n2: number) =>  n1 + n2;
+add = (n1: number, n2: number) => n1 + n2;
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string; //선택적 매개변수
 }
 
 //인터페이스 상속
-interface Greetable extends Named{
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 //인터페이스 구현
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);  
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
   }
 }
 
 //인터페이스 타입으로 변수 선언
 let user1: Greetable;
-user1 = new Person('Max');
+user1 = new Person();
 user1.greet("Hi there - I am");
