2675cb27b2a158a775666a45e7769e2af56afdfb
youngil <youngil@placeb.com>
2024-06-01 13:44:51 +0900

    함수 타입으로서 인터페이스

--- class-study/src/app.ts
+++ class-study/src/app.ts
@@ -1,11 +1,20 @@
+//type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+let add: AddFn;
+add = (n1: number, n2: number) =>  n1 + n2;
+
 interface Named {
   readonly name: string;
 }
 
+//인터페이스 상속
 interface Greetable extends Named{
   greet(phrase: string): void;
 }
 
+//인터페이스 구현
 class Person implements Greetable {
   name: string;
   age = 30;
@@ -19,6 +28,7 @@ class Person implements Greetable {
   }
 }
 
+//인터페이스 타입으로 변수 선언
 let user1: Greetable;
 user1 = new Person('Max');
 user1.greet("Hi there - I am");
