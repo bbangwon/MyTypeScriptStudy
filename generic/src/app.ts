7eb2c9160c11c607926c1728fabda02f940eae49
BreadOne <bbangwon.chung@gmail.com>
2024-06-02 08:20:07 +0900

    제약조건

--- generic/src/app.ts
+++ generic/src/app.ts
@@ -14,9 +14,9 @@ promise.then(data => {
 });
 
 // 제너릭 함수
-function merge<T extends {}, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, {age: 30});
 console.log(mergedObj);
