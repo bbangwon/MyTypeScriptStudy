28b098c12166590e7fcd7e2f8e10299b62739a8d
youngil <youngil@placeb.com>
2024-06-05 18:45:14 +0900

    Google API 키 설정하기

--- demo-address/src/app.ts
+++ demo-address/src/app.ts
@@ -1,6 +1,8 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyCH2lhuw3aRTXCdZNKlt6WrN8l2xOZHlDk';
+
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
@@ -8,6 +10,8 @@ function searchAddressHandler(event: Event) {
     // send this to Google's API!
 
 }
+
+
 
 form.addEventListener('submit', searchAddressHandler);
     
