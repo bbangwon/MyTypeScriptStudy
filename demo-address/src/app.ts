3689a43af7365f0e2aab77439e4267d6e264a7e4
youngil <youngil@placeb.com>
2024-06-05 18:36:50 +0900

    사용자 입력 받기

--- demo-address/src/app.ts
+++ demo-address/src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!!');
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    // send this to Google's API!
+
+}
+
+form.addEventListener('submit', searchAddressHandler);
+    
+    
