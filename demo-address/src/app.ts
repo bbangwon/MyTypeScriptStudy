e1d12fe530bf5ea6c6a6ecdeeaa59662e8bf3cc9
youngil <youngil@placeb.com>
2024-06-05 19:09:34 +0900

    Google 지도로 지도 렌더링

--- demo-address/src/app.ts
+++ demo-address/src/app.ts
@@ -1,18 +1,45 @@
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+import axios from "axios";
 
-const GOOGLE_API_KEY = 'AIzaSyCH2lhuw3aRTXCdZNKlt6WrN8l2xOZHlDk';
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyCH2lhuw3aRTXCdZNKlt6WrN8l2xOZHlDk";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
-    event.preventDefault();
-    const enteredAddress = addressInput.value;
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
 
-    // send this to Google's API!
+  // send this to Google's API!
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 16,
+      });
 
+      new google.maps.Marker({
+        position: coordinates,
+        map
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-
-
-form.addEventListener('submit', searchAddressHandler);
-    
-    
+form.addEventListener("submit", searchAddressHandler);
